fdab7af547efdd6920143efd4a3fccb9fbcd2bde
Brian Olsen <btolsen131@gmail.com>
2022-04-30 18:55:10 -0400

    adding new post feature

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.CharField(max_length=140)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
 
 def home(request):
     context = {
         'posts':Post.objects.all()
     }
     return render(request, 'blog/home.html', context) 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['content']
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
