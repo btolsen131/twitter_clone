0d618c5ececd8c1b4021d411f52df96645803b17
Brian Olsen <btolsen131@gmail.com>
2022-04-30 14:16:09 -0400

    Fixed media root to fix profile picture function

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -132,5 +132,5 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
-MEDIA_ROUTE = BASE_DIR.joinpath('media')
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
