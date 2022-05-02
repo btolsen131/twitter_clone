c0be80a0507a225e333c7a3296e053b815ffe902
Brian Olsen <btolsen131@gmail.com>
2022-05-01 21:18:07 -0400

    Added pagination and start of profile pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>',UserPostListView.as_view(), name='user-posts'),
     path('about/', views.about, name='blog-about'),
 ]
