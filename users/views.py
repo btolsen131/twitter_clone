4098cf56cba6e7f7a20c5cd028f09847a5f3ea7d
Brian Olsen <btolsen131@gmail.com>
2022-05-12 17:37:26 -0400

    added profile page editting

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import View
+from django.views.generic import View, UpdateView
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from blog.models import Post
@@ -28,13 +28,32 @@ class ProfileView(View):
         profile = Profile.objects.get(pk=pk)
         user = profile.user
         posts = Post.objects.filter(author=user).order_by('-date_posted')
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if request.method == "POST":
+            u_form = UserUpdateForm(request.POST, instance=request.user)
+            p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+            if u_form.is_valid() and p_form.is_valid():
+                u_form.save()
+                p_form.save()
+                messages.success(request, f'Account updated!')
+                return redirect('profile')
+            else:
+                u_form = UserUpdateForm(instance=user)
+                p_form = ProfileUpdateForm(instance=user.profile)
 
         context = {
             'user':user,
             'profile':profile,
-            'posts':posts
+            'posts':posts,
+            'u_form':u_form,
+            'p_form':p_form,
         }
+
+        
         return render(request, 'users/profile.html', context)
+
+
 
 
 """ @login_required
